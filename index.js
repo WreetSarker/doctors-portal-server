f4e3825fc4dee2cff49e052ad301a506ecc05074
WreetSarker <wreet.sarker@gmail.com>
2021-04-12 10:14:20 +0600

    created a database in mongo db and added integration with it

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ require('dotenv').config();
 
 
 
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kmggs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const appointmentsCollection = client.db("doctorsPortal").collection("dpCollection");
+
+    app.post('/addAppointment', (req, res) => {
+        const appointment = req.body;
+        appointmentsCollection.insertOne(appointment)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+});
+
+
 app.get('/', (req, res) => {
     res.send('Server Opened')
 })
